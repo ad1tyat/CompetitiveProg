cdd6ef9520546924ef8d92d2a7e419f1948377e1
Aditya Trivedi <30773534+ad1tyat@users.noreply.github.com>
2020-05-23 17:48:24 +0530

    Create Sum of Two Values-1640.cpp

---
+++ CSES/Sum of Two Values-1640.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define M1 1000000007
+#define M2 998244353
+#define ll long long
+#define pll pair<ll,ll>
+#define REP(i,a,b) for(ll i=a;i<b;i++)
+#define REPR(i,a,b) for(ll i=b-1;i>=a;i--)
+#define forr(i,n) for(ll i=0;i<n;i++)
+#define F first
+#define S second
+#define pb push_back
+#define DB pop_back
+#define mp make_pair
+#define MT make_tuple
+#define V(a) vector<a>
+#define vi vector<ll>
+#define endl '\n'
+#define ce(ele) cout<<ele<<' '
+#define cs(ele) cout<<ele<<'\n'
+#define CASE(t) ll t; cin>>t; while(t--)
+
+void FAST() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+int main() {
+	FAST();
+	ll n, req;
+	cin >> n >> req;
+	vector<pair<ll, ll>> arr;
+	ll ori[n];
+	for (int i = 0; i < n; i++) {
+		ll ele;
+		cin >> ele;
+		arr.pb(mp(ele, i));
+	}
+	sort(arr.begin(), arr.end());
+	// for (auto x : arr) {
+	// 	cout << x.first << " " << x.second << endl;
+	// }
+	ll lo = 0, hi = n - 1;
+	ll f = 0;
+	while (lo < hi) {
+		if (arr[lo].first + arr[hi].first == req) {
+			cout << arr[lo].second + 1 << " " << arr[hi].second + 1;
+			f = 1;
+			break;
+		}
+		else if (arr[lo].first + arr[hi].first < req) {
+			lo++;
+		}
+		else {
+			hi--;
+		}
+	}
+	if (!f)
+		cout << "IMPOSSIBLE";
+}
