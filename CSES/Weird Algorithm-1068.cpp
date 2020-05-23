6548c09461ae068bf0077c0e597bfbe4ce922cb7
Aditya Trivedi <30773534+ad1tyat@users.noreply.github.com>
2020-05-23 16:42:44 +0530

    Create Weird Algorithm-1068.cpp

---
+++ CSES/Weird Algorithm-1068.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+	__________   _____     _________   __            __    __________    ________     _________
+	----------  | ----\    ---------   \ \   		/ /    |  _______|	| _____  \    ---------
+		||      ||    ||  	  ||		\ \		   / /	   | |			| |		\ \	     ||
+		||      ||____//      ||		 \ \      / /	   | |_____ 	| |		 | |	 ||
+		||      |__ __/       ||		  \ \    / /	   | _____|		| |		 | |	 ||
+		||      || \\         ||		   \ \  / /	   	   | |			| |		 | |	 ||
+		||      ||  \\     ___||____        \ \/ /         | |_______ 	| |_____/ /   ___||____
+		||		|| 	 \\    _________         \__/		   |_________|	|________/	  _________
+*/
+/********************************************************************/
+/*
+Codeforces : @a.trivedi
+Codechef   : @adityat25
+HackerRank : @adityatrivedi
+ 
+/********************************************************************/
+#define M1 1000000007
+#define M2 998244353
+#define ll long long
+#define pll pair<ll,ll>
+#define REP(i,a,b) for(ll i=a;i<b;i++)
+#define REPR(i,a,b) for(ll i=b-1;i>=a;i--)
+#define forr(i,n) for(ll i=0;i<n;i++)
+#define F first
+#define S second
+#define pb push_back
+#define DB pop_back
+#define mp make_pair
+#define MT make_tuple
+#define V(a) vector<a>
+#define vi vector<ll>
+#define endl '\n'
+#define ce(ele) cout<<ele<<' '
+#define cs(ele) cout<<ele<<'\n'
+#define CASE(t) ll t; cin>>t; while(t--)
+/********************************************************************/
+const double pi = 3.1415926535;
+/********************************************************************/
+//FAST IO//
+void FAST() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+/********************************************************************/
+int main() {
+	FAST();
+	ll n;
+	cin >> n;
+	while (n != 1) {
+		cout << n << " ";
+		if (n & 1)
+			n = 3 * n + 1;
+		else
+			n /= 2;
+	}
+	cout << 1;
+ 
+	return 0;
+}
